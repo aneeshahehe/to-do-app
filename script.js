4806afbee1df4be230348adfad8649686f1ee417
aneeshahehe <aneeshabanik07@gmail.com>
2024-07-25 16:09:06 +0530

    2nd commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// // document.getElementById()    /* gets elements by their IDs */
+
+// let todos = [];
+
+// const todosList = document.getElementById('todos');  //access todos section from html in JS
+// function renderTodos()   // it takes all the todos from the array and appends it to the html todos section
+// {
+//     todos.forEach((todo, i) => 
+//     {
+//         let currentHTML = todoList.innerHTML
+//         let newHTML =
+//         {
+//             `<div class="todoItem">
+//             `
+//         }
+//     })
+// }
